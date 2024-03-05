7b344a8b0fccad309b4df2fe417d115689b82d52
Yoanna Popova <yoanna.dp@gmail.com>
2024-03-05 14:49:50 +0200

    Create login component and add style settings.

--- bookshelf/src/app/layouts/layouts.module.ts
+++ bookshelf/src/app/layouts/layouts.module.ts
@@ -3,19 +3,22 @@ import { CommonModule } from '@angular/common';
 
 import { HomeComponent } from './home/home.component';
 import { ContactsComponent } from './contacts/contacts.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    ContactsComponent
+    ContactsComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     HomeComponent,
-    ContactsComponent
+    ContactsComponent,
+    LoginComponent
   ]
 })
 export class LayoutsModule { }
