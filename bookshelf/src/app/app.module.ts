ae1f34570e394b46a27a09474343386dd224814c
Yoanna Popova <yoanna.dp@gmail.com>
2024-03-05 10:22:20 +0200

    Create modul layouts and add stile to home view.

--- bookshelf/src/app/app.module.ts
+++ bookshelf/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+
 import { CoreModule } from './core/core.module';
+import { LayoutsModule } from './layouts/layouts.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { CoreModule } from './core/core.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CoreModule
+    CoreModule,
+    LayoutsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
