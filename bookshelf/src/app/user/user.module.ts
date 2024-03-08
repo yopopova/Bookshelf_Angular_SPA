d10f004d6c2b2a8a0c41a7bc6fd33bd58028ba04
Yoanna Popova <yoanna.dp@gmail.com>
2024-03-08 14:42:05 +0200

    Change app menu and create frofile structure and design.

--- bookshelf/src/app/user/user.module.ts
+++ bookshelf/src/app/user/user.module.ts
@@ -3,19 +3,22 @@ import { CommonModule } from '@angular/common';
 
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 @NgModule({
   declarations: [
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    ProfileComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    ProfileComponent
   ]
 })
 export class UserModule { }
