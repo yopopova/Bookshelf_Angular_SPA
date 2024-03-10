52541903d0981bd925984a60701f890c2decefc0
Yoanna Popova <yoanna.dp@gmail.com>
2024-03-10 18:38:09 +0200

    Move 404 view to root folder and set its router up.

--- bookshelf/src/app/app-routing.module.ts
+++ bookshelf/src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/home' },
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: '**', redirectTo: '/404' },
+  { path: '404', component: PageNotFoundComponent }
 ];
 
 @NgModule({
--- bookshelf/src/app/app.module.ts
+++ bookshelf/src/app/app.module.ts
@@ -4,27 +4,27 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 import { CoreModule } from './core/core.module';
 import { LayoutsModule } from './layouts/layouts.module';
 import { BooksModule } from './books/books.module';
 import { UserModule } from './user/user.module';
-import { SharedModule } from './shared/shared.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     CoreModule,
     LayoutsModule,
     BooksModule,
     UserModule,
-    SharedModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
