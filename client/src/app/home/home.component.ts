477fc52284078debcf4a8ad821bf9b58a1ccea3a
Yoanna Popova <yoanna.dp@gmail.com>
2024-09-12 08:47:36 +0300

    Set data up for home component.

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+    constructor(private api: ApiService) {}
 
+    ngOnInit(): void {
+        this.api.getLatest().subscribe(latestBooks => {
+            console.log(latestBooks);
+        });
+    }
 }
