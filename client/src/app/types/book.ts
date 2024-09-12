822d45f437c489faea1643597df68dd97d509ee1
Yoanna Popova <yoanna.dp@gmail.com>
2024-09-12 08:21:04 +0300

    Create book type.

---
+++ client/src/app/types/book.ts
@@ -0,0 +1,12 @@
+export interface Book {
+    _ownerId: string;
+    title: string;
+    cover: string;
+    author: string;
+    year: string;
+    pages: string;
+    price: string;
+    description: string;
+    _createdOn: number;
+    _id: string;
+}
