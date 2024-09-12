ec45e1222b3b9152d70d343bb87f4a5e866da407
Yoanna Popova <yoanna.dp@gmail.com>
2024-09-12 08:48:00 +0300

    Set data up for catalog component.

--- client/src/app/books/catalog/catalog.component.ts
+++ client/src/app/books/catalog/catalog.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/api.service';
 
 @Component({
   selector: 'app-catalog',
   templateUrl: './catalog.component.html',
   styleUrls: ['./catalog.component.css']
 })
-export class CatalogComponent {
+export class CatalogComponent implements OnInit {
+    constructor(private api: ApiService) {}
 
+    ngOnInit(): void {
+        this.api.getAllBooks().subscribe(books => {
+            console.log(books);            
+        });
+    }
 }
