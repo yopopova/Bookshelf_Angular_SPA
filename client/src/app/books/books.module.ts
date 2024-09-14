8d54ce8d02b1036b388d2a75a6116c79378c6a99
Yoanna Popova <yoanna.dp@gmail.com>
2024-09-14 13:03:17 +0300

    Initialize book details component.

--- client/src/app/books/books.module.ts
+++ client/src/app/books/books.module.ts
@@ -6,11 +6,13 @@ import { BooksRoutingModule } from './books-routing.module';
 import { CatalogComponent } from './catalog/catalog.component';
 import { AddBookComponent } from './add-book/add-book.component';
 import { SharedModule } from '../shared/shared.module';
+import { BookDetailsComponent } from './book-details/book-details.component';
 
 @NgModule({
   declarations: [
     CatalogComponent,
-    AddBookComponent
+    AddBookComponent,
+    BookDetailsComponent
   ],
   imports: [
     CommonModule,
