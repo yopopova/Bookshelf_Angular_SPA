3842271189ada1041629ed1e65ef119e9b0fd18f
Yoanna Popova <yoanna.dp@gmail.com>
2024-11-11 09:47:01 +0200

    Set up add book settings.

--- client/src/app/books/add-book/add-book.component.ts
+++ client/src/app/books/add-book/add-book.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/api.service';
+import { Book } from 'src/app/types/book';
 
 @Component({
   selector: 'app-add-book',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-book.component.css']
 })
 export class AddBookComponent {
+  constructor(private apiService: ApiService) {}
 
+  addBook(ev: Event, inputTitle: string, inputCover: string, inputAuthor: string, inputYear: string, inputPages: string, inputPrice: string, inputDescription: string) {
+    ev.preventDefault();
+
+    this.apiService.createBook(inputTitle, inputCover, inputAuthor, inputYear, inputPages, inputPrice, inputDescription).subscribe(data => {
+      console.log(data);
+    });
+  }
 }
