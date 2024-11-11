237da15751c0855b7a83de2ed070fe94e6cbdaa4
Yoanna Popova <yoanna.dp@gmail.com>
2024-11-11 09:48:20 +0200

    Set book details component up.

--- client/src/app/books/book-details/book-details.component.ts
+++ client/src/app/books/book-details/book-details.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from 'src/app/api.service';
+import { Book } from 'src/app/types/book';
 
 @Component({
   selector: 'app-book-details',
   templateUrl: './book-details.component.html',
   styleUrls: ['./book-details.component.css']
 })
-export class BookDetailsComponent {
+export class BookDetailsComponent implements OnInit {
+  book = {} as Book;
 
+  constructor(
+    private apiService: ApiService,
+    private activeRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.activeRoute.params.subscribe((data) => {
+      const id = data['bookId'];
+
+      this.apiService.getSingleBook(id).subscribe((book) => {
+        this.book = book;
+
+        console.log({ book });
+        
+      });
+    });
+
+  }
 }
