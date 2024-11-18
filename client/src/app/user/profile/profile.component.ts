f066e8942a8e9a379d306a6bc0748565c1d9cd00
Yoanna Popova <yoanna.dp@gmail.com>
2024-11-18 11:17:06 +0200

    Set up user profile component's business logic and form validation.

--- client/src/app/user/profile/profile.component.ts
+++ client/src/app/user/profile/profile.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+import { ProfileDetails } from 'src/app/types/user';
+import { emailValidator } from 'src/app/shared/utils/email-validator';
+import { EMAIL_DOMAINS } from 'src/app/constants';
 
 @Component({
   selector: 'app-profile',
@@ -6,5 +11,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
+  showEditMode: boolean = false;
 
+  profileDetails: ProfileDetails = {
+    username: 'Ivan',
+    photo: 'link',
+    city: 'Vidin',
+    email: 'ivan@gmail.com',
+  };
+
+  form = this.fb.group({
+    username: ['', [Validators.required, Validators.minLength(3)]],
+    photo: ['', [Validators.required]],
+    city: ['', [Validators.required]],
+    email: ['', [Validators.required, emailValidator(EMAIL_DOMAINS)]],
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  onToggle(): void {
+    this.showEditMode = !this.showEditMode;
+  }
+
+  saveProfileHandler(): void {
+    if (this.form.invalid) {
+      return;
+    }
+
+    this.profileDetails = this.form.value as ProfileDetails;
+    this.onToggle();
+  }
 }
