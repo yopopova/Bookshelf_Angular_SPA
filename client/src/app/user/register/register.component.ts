ef0b128d9413b24154d559bfa1c4636c6c48539f
Yoanna Popova <yoanna.dp@gmail.com>
2024-11-19 11:48:42 +0200

    Set up register form validations.

--- client/src/app/user/register/register.component.ts
+++ client/src/app/user/register/register.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+import { EMAIL_DOMAINS } from 'src/app/constants';
+import { emailValidator } from 'src/app/shared/utils/email-validator';
+import { matchPasswordsValidator } from 'src/app/shared/utils/match-passwords-validator';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +11,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  form = this.fb.group({
+    // controls
+    username: ['', [Validators.required, Validators.minLength(3)]],
+    photo: ['', [Validators.required]],
+    city: ['', [Validators.required]],
+    email: ['', [Validators.required, emailValidator(EMAIL_DOMAINS)]],
+    passGroup: this.fb.group(
+      {
+        password: ['', [Validators.required]],
+        rePassword: ['', [Validators.required]],
+      }, 
+      {
+        validators: [matchPasswordsValidator('password', 'rePassword')],
+      })
+  });
 
+  constructor(private fb: FormBuilder) {}
+
+  register(): void {
+    if (this.form.invalid) {
+      return;
+    }
+
+    console.log(this.form.value);
+  }
 }
