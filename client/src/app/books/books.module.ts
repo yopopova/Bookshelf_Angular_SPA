c41cbacf2a665578c0ec59d260f0c316e66a7f98
Yoanna Popova <yoanna.dp@gmail.com>
2024-11-20 11:33:47 +0200

    Add SharedModule and FormsModule to the book module.

--- client/src/app/books/books.module.ts
+++ client/src/app/books/books.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { BooksRoutingModule } from './books-routing.module';
 
 import { CatalogComponent } from './catalog/catalog.component';
 import { AddBookComponent } from './add-book/add-book.component';
-import { SharedModule } from '../shared/shared.module';
 import { BookDetailsComponent } from './book-details/book-details.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { BookDetailsComponent } from './book-details/book-details.component';
   imports: [
     CommonModule,
     BooksRoutingModule,
-    SharedModule
+    SharedModule,
+    FormsModule,
   ],
   exports: [
     CatalogComponent,
