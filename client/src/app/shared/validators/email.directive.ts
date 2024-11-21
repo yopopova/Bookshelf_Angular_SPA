68f46827705226b2fdfdf752a04399418dc400d2
Yoanna Popova <yoanna.dp@gmail.com>
2024-11-21 15:39:14 +0200

    Create email directive.

---
+++ client/src/app/shared/validators/email.directive.ts
@@ -0,0 +1,25 @@
+import { Directive, Input } from '@angular/core';
+import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator, ValidatorFn } from '@angular/forms';
+
+import { emailValidator } from '../utils/email-validator';
+
+@Directive({
+  selector: '[appEmail]',
+  providers: [
+    {
+      provide: NG_VALIDATORS,
+      useExisting: EmailDirective,
+      multi: true,
+    }
+  ]
+})
+export class EmailDirective implements Validator {
+  @Input() appEmail: string[] = [];
+  
+  constructor() {}
+
+  validate(control: AbstractControl<any, any>): ValidationErrors | null { 
+    const validatorFn = emailValidator(this.appEmail);
+    return validatorFn(control);
+  }
+}
