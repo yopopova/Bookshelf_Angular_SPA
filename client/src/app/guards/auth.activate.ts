ea7f3f07a6178caad1c3d2bf1a5e7c8926453bf7
Yoanna Popova <yoanna.dp@gmail.com>
2024-12-06 12:13:22 +0200

    Prevent not logged in user to access the profile page.

--- client/src/app/guards/auth.activate.ts
+++ client/src/app/guards/auth.activate.ts
@@ -1,19 +1,24 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+
 import { UserService } from '../user/user.service';
 
-@Injectable({ providedIn: 'root' })
+@Injectable({
+  providedIn: 'root'
+})
 export class AuthActivate implements CanActivate {
-    constructor(private userService: UserService) {}
+    constructor(private userService: UserService, private router: Router) {}
 
     canActivate(
       route: ActivatedRouteSnapshot,
       state: RouterStateSnapshot
     ):
+      | boolean 
+      | UrlTree
       | Observable<boolean | UrlTree>
-      | Promise<boolean | UrlTree>
-      | boolean | UrlTree {
+      | Promise<boolean | UrlTree> {
+        this.router.navigate(['login']);
         return this.userService.isLogged;
       }
 }
--- client/src/app/user/user-routing.module.ts
+++ client/src/app/user/user-routing.module.ts
@@ -4,15 +4,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ProfileComponent } from './profile/profile.component';
+import { AuthActivate } from '../guards/auth.activate';
 
 const routes: Routes = [
-    {path: 'login', component: LoginComponent},
-    {path: 'register', component: RegisterComponent},
-    {path: 'profile', component: ProfileComponent}
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'profile', component: ProfileComponent, canActivate: [AuthActivate] }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class UserRoutingModule { }
+export class UserRoutingModule {}
