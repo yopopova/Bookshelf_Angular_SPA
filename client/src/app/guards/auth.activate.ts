9d7bca25d4f901969f4b50c0dfef0f743fe2bfcb
Yoanna Popova <yoanna.dp@gmail.com>
2024-12-13 08:51:08 +0200

    Set up the route guards.

--- client/src/app/guards/auth.activate.ts
+++ client/src/app/guards/auth.activate.ts
@@ -18,7 +18,7 @@ export class AuthActivate implements CanActivate {
       | UrlTree
       | Observable<boolean | UrlTree>
       | Promise<boolean | UrlTree> {
-        this.router.navigate(['login']);
+        // this.router.navigate(['login']);
         return this.userService.isLogged;
       }
 }
