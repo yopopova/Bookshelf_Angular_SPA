8bed66e1be2042fa2859d5bddd3c2003572f73f0
Yoanna Popova <yoanna.dp@gmail.com>
2024-12-13 08:44:56 +0200

    Set up the user service.

--- client/src/app/user/user.service.ts
+++ client/src/app/user/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Subscription, tap } from 'rxjs';
+// import { HttpHeaders } from '@angular/common/http';
 
 import { UserForAuth } from '../types/user';
 import { environment } from 'src/environments/environment.development';
@@ -18,12 +19,20 @@ export class UserService implements OnDestroy {
   userSubscription: Subscription;
 
   get isLogged(): boolean {
-    console.log(this.user);
-    
+    console.log(this.user?.accessToken);
     return !!this.user;
   }
 
+  // console.log(this.user);
+  
   constructor(private http: HttpClient) {
+    // try {
+    //   const lsUser = localStorage.getItem(this.USER_KEY) || '';
+    //   this.user = JSON.parse(lsUser);
+    // } catch (error) {
+    //   this.user = undefined;
+    // }
+
     this.userSubscription = this.user$.subscribe(user => {
       this.user = user;
     })
@@ -31,6 +40,7 @@ export class UserService implements OnDestroy {
 
   login(email: string, password: string) {
     const { apiUrl } = environment;
+    // localStorage.setItem(this.USER_KEY, JSON.stringify(this.user));
 
     return this.http.post<UserForAuth>(`${apiUrl}/users/login`, { email, password }).pipe(
       tap((user) => {
@@ -41,6 +51,8 @@ export class UserService implements OnDestroy {
 
   register(username: string, photo: string, city: string, email: string, password: string, rePassword: string) {
     const { apiUrl } = environment;
+    // localStorage.setItem(this.USER_KEY, JSON.stringify(this.user));
+
 
     return this.http.post<UserForAuth>(`${apiUrl}/users/register`, { username, photo, city, email, password, rePassword }).pipe(
       tap((user) => {
@@ -50,13 +62,31 @@ export class UserService implements OnDestroy {
   }
 
   logout() {
+    const { apiUrl } = environment;
+    // localStorage.clear();
     // this.user = undefined;
     // localStorage.removeItem(this.USER_KEY);
 
-    const { apiUrl } = environment;
-
     return this.http.post(`${apiUrl}/users/logout`, {})
       .pipe(tap(() => this.user$$.next(undefined)));
+  }
+
+  // TO CHECK THIS FUNCTIONALITY !!!
+  getProfile() {
+    const { apiUrl } = environment;
+    // const userId = this.user?._id;
+    // console.log(this.user?._id);
+    const token = this.user?.accessToken;
+    
+    // return this.user$;
+    // return this.http.get<UserForAuth>(`${apiUrl}/users/${this.user?._id}`).pipe(tap(user => this.user$$.next(user)));
+    // return this.http.get<UserForAuth>(`${apiUrl}/users/me`).pipe(tap(user => this.user$$.next(user)));
+
+    return this.http.get<UserForAuth>(`${apiUrl}/users/me`).pipe(tap(user => this.user$$.next(user)));
+  }
+
+  get getToken() {
+    return this.user?.accessToken;
   }
 
   ngOnDestroy(): void {
