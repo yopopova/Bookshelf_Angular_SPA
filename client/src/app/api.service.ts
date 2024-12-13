485f37e84d3c31ecf23987e59162889fa8a0cfa8
Yoanna Popova <yoanna.dp@gmail.com>
2024-12-13 08:42:02 +0200

    Add edit and delete book functionality into the api service.

--- client/src/app/api.service.ts
+++ client/src/app/api.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
 import { environment } from 'src/environments/environment.development';
 import { Book } from './types/book';
 
@@ -11,33 +12,52 @@ export class ApiService {
 
   getAllBooks() {
     const { apiUrl } = environment;
-    return this.http.get<Book[]>(`${apiUrl}/books`);
+    return this.http.get<Book[]>(`${apiUrl}/data/books`);
+  }
+
+  getLatest() {
+    const { apiUrl } = environment;
+    return this.http.get<Book[]>(`${apiUrl}/data/books?sortBy=_createdOn%20desc&offset=0&pageSize=3`);
   }
 
   getSingleBook(id: string) {
-    // TODO: implement it later
     const { apiUrl } = environment;
-    return this.http.get<Book>(`${apiUrl}/books/${id}`);
+    return this.http.get<Book>(`${apiUrl}/data/books/${id}`);
   }
 
-  createBook(title: string, cover: string, author: string, year: string, pages: string, price: string, description: string) {
+  createBook(title: string, cover: string, author: string, pages: string, price: string, description: string) {
     const { apiUrl } = environment;
 
     const payload = {
       title,
       cover,
       author,
-      year,
       pages,
       price,
       description
     }
 
-    return this.http.post<Book>(`${apiUrl}/books`, payload);
+    return this.http.post<Book>(`${apiUrl}/data/books`, payload);
   }
 
-  getLatest() {
+  updateBook(id: string, title: string, cover: string, author: string, pages: string, price: string, description: string) {
     const { apiUrl } = environment;
-    return this.http.get<Book[]>(`${apiUrl}/books?sortBy=_createdOn%20desc&offset=0&pageSize=3`);
+
+    const payload = {
+      title,
+      cover,
+      author,
+      pages,
+      price,
+      description
+    }
+
+    return this.http.put<Book>(`${apiUrl}/data/books/${id}`, payload);
   }
+
+  removeBook(id: string) {
+    const { apiUrl } = environment;
+    return this.http.delete<Book>(`${apiUrl}/data/books/${id}`);
+  }
+
 }
