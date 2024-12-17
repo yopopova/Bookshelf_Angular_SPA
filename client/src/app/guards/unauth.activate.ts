bb3b2f69fe020895026280f04d3a64834c2ee214
Yoanna Popova <yoanna.dp@gmail.com>
2024-12-17 09:50:02 +0200

    Create unauthenticated route guard.

---
+++ client/src/app/guards/unauth.activate.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { UserService } from '../user/user.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UnauthActivate implements CanActivate {
+    constructor(private userService: UserService, private router: Router) {}
+
+    canActivate(
+      route: ActivatedRouteSnapshot,
+      state: RouterStateSnapshot
+    ):
+      | boolean 
+      | UrlTree
+      | Observable<boolean | UrlTree>
+      | Promise<boolean | UrlTree> {
+
+        if (!this.userService.isLogged) {
+            this.router.navigate(['login']);
+            return false;
+        } else {
+            return true;
+        }
+        
+        // return this.userService.isLogged;
+      }
+}
